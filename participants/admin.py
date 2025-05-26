25ab8ba4a5ebba9207bcc134ccb499ef211eaa3b
student <student@iab.kz>
2025-05-26 18:48:15 +0600

    Fix style and add search in admin

--- participants/admin.py
+++ participants/admin.py
@@ -3,4 +3,6 @@ from .models import Participant
 
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'uuid']
+    ordering = ['-points']
     list_display = ('uuid', 'name', 'points')
