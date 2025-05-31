04339ad9081544a025aa55d10b1cd9f4ac38db32
student <student@iab.kz>
2025-06-01 05:07:54 +0600

    removed index

--- participants/urls.py
+++ participants/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('participant/<uuid:uuid>/', views.participant_detail, name='participant_detail'),
 ]
