4a1dd3ebcc792650e998cb3bc43c7d0b6347ca17
student <student@iab.kz>
2025-05-31 21:33:04 +0600

    Added ranking to some games

--- balafest_1/settings.py
+++ balafest_1/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +91,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
--- participants/models.py
+++ participants/models.py
@@ -21,6 +21,7 @@ class Game(models.Model):
     name = models.CharField(max_length=100)
     points = models.PositiveIntegerField()
     age_group = models.CharField(max_length=10, choices=AGE_GROUP_CHOICES)
+    is_ranked = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.name} ({self.points} pts)"
@@ -32,3 +33,21 @@ class GameCompletion(models.Model):
 
     class Meta:
         unique_together = ('participant', 'game')
+
+class GamePlacement(models.Model):
+    PLACE_CHOICES = [
+        (1, '1st Place'),
+        (2, '2nd Place'),
+        (3, '3rd Place'),
+    ]
+
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='placements')
+    place = models.IntegerField(choices=PLACE_CHOICES)
+    points_awarded = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('participant', 'game')
+
+    def __str__(self):
+        return f"{self.participant.name} - {self.game.name} - {self.get_place_display()}"
